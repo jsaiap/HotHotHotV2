018059dea021327b446b979ece85068009940d46
FERRALI-Mario <mario.ferrali@etu.univ-amu.fr>
2021-12-08 17:05:41 +0100

    Ajout des fonctionnalités permettant de changer et de checker dans la bdd le nom, l'email et le mot de passe

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,28 +10,35 @@ final class User extends ObjectModel{
     private $connection_date;
 	private $admin;
 
+	/**
+     * Affiche les informations de l'utilisateur
+     */
     public function showMembers() {
         $req = prepare('SELECT `name`, `email`, creation_date, `admin` FROM users');
         return execute($req);
     }
 
+    /**
+     * Permet d'ajouter un membre à la base de données
+     */
     public function createMember($user_id, $name, $email, $password, $creation_date, $connection_date, $admin = 0) {
-        $req = prepare('INSERT INTO users ($user_id, $name, $email, $password, $creation_date, $connection_date) VALUES(?,?,?,?,?,NOW())');
+        $req = prepare('INSERT INTO users ($user_id, $name, $email, $password, $creation_date, $connection_date, $admin) VALUES(?,?,?,?,?,NOW(),?)');
         return execute($req);
     }
 
-    public function changePassword($user_id, $password) {
-        $req = prepare('UPDATE users SET password = '.$password.' WHERE id = '.$user_id);
-        $req = execute($req);
+    /**
+     * Fonction qui regarde dans la base de données si la donnée existe (nom ou email ou mot de passe)
+     */
+    public function checkData($data) {
+        $req = prepare('SELECT `'.$data.'` FROM users WHERE');
+        return execute($req);
     }
 
-    public function changeName($user_id, $name) {
-        $req = prepare('UPDATE users SET `name` = '.$name.' WHERE id = '.$user_id);
-        $req = execute($req);
-    }
-
-    public function changeEmail($user_id, $email) {
-        $req = prepare('UPDATE users SET `email` = '.$email.' WHERE id = '.$user_id);
-        $req = execute($req);
+    /**
+     * Fonction qui change la donnée (nom ou email ou mot de passe)
+     */
+    public function changeData($data, $password, $user_id) {
+        $req = prepare('UPDATE users SET '.$data.' = '.$password.' WHERE id = '.$user_id);
+        return execute($req);
     }
 }
