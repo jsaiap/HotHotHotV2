e3e1d05a2dad43ad75c3a3edab66d891b3635bc7
Vincent Asta <62051465+Vixxs@users.noreply.github.com>
2022-02-13 13:17:59 +0100

    Fix css

--- app/Views/panel/page/documentation_create.php
+++ app/Views/panel/page/documentation_create.php
@@ -2,11 +2,11 @@
 <main id="main">
     <section class="main-section">
         <h2 class="page-name">Creation de documentation</h2>
-        <form class="form-doc" action="/documentation/create" method="POST">
+        <form class="w-70 form-doc" action="/documentation/create" method="POST">
             <label for="title">Titre</label>
             <input type="text" name="title" placeholder="">
             <label for="text">Text</label>
-            <textarea name="text" cols="50" rows="30" placeholder="Documentation ici"></textarea>
+            <textarea name="text" rows="30" placeholder="Documentation ici"></textarea>
             <button class="end table-btn" type="submit">Créer</button>
         </form>
     </section>
--- app/Views/panel/page/documentation_detail.php
+++ app/Views/panel/page/documentation_detail.php
@@ -2,18 +2,18 @@
 <main id="main">
     <section class="main-section">
         <h2 class="page-name">Documentation</h2>
-         <table>
+         <table class="w-70">
             <form action="/documentation/update" method="POST">
                 <input type="hidden" name="id" value="<?php echo $view['documentation']->id ?>">
                 <thead>
                     <tr>
-                        <th><input class="title-input" type="text" name="title" value="<?php echo $view['documentation']->title ?>"></th>
+                        <th><input class="input-responsive title-input" type="text" name="title" value="<?php echo $view['documentation']->title ?>"></th>
                     </tr>
                 </thead>
                 <tbody>
                     
                     <tr>
-                        <td><textarea name="text" cols="50" rows="35"><?php echo $view['documentation']->text ?></textarea></td>
+                        <td><textarea class="textarea-responsive" name="text" rows="35"><?php echo $view['documentation']->text ?></textarea></td>
                     </tr>
                     <tr>
                         <td><button class="table-btn-outlined" type="submit">Modifier</button> </td>
